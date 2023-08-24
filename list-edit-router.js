8da20bf5bd4c35498447f8e0f811cdff6b7eed25
Alex Peña <andriuspena666@gmail.com>
2023-08-24 16:09:47 -0500

    Agrega rutas con Express Router para el proyecto 2.1

--- list-edit-router.js
+++ list-edit-router.js
@@ -2,18 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 // Ruta POST para crear una nueva tarea
-router.post('/create-task', (req, res) => {
-  // Aquí implementa la lógica para crear una nueva tarea
+router.post('/create-task', (req, res) => { 
 });
 
 // Ruta DELETE para eliminar una tarea
 router.delete('/delete-task/:taskId', (req, res) => {
-  // Aquí implementa la lógica para eliminar una tarea
 });
 
 // Ruta PUT para actualizar una tarea
 router.put('/update-task/:taskId', (req, res) => {
-  // Aquí implementa la lógica para actualizar una tarea
 });
 
 module.exports = router;
--- list-view-router.js
+++ list-view-router.js
@@ -3,12 +3,10 @@ const router = express.Router();
 
 // Ruta GET para listar tareas completas
 router.get('/completed-tasks', (req, res) => {
-  // Aquí implementa la lógica para listar tareas completas
 });
 
 // Ruta GET para listar tareas incompletas
 router.get('/incomplete-tasks', (req, res) => {
-  // Aquí implementa la lógica para listar tareas incompletas
 });
 
 module.exports = router;
